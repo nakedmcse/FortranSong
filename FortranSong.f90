e6fb0eb72dbbadb88e3441590e6ab4216d49af03
Walker Aldridge <daldridge@lairdscomputer.com>
2023-09-25 15:18:57 -0500

    Get Month and Year to use automatically

--- FortranSong.f90
+++ FortranSong.f90
@@ -9,12 +9,17 @@ program Song
     character(kind=json_ck,len=:),allocatable :: message
     character(len=80) :: apiKey
     character(len=2048) :: query
+    character(len=14) :: monthYear
 
     !Get API Key from environment variable
     call GetAPIKey(apiKey)
 
+    !Get Month and Year to use
+    call GetMonthYear(monthYear)
+
     !Call API for completion
-    query = 'Trying to create a music video challenge for the month (September 2023) and would like you to return a theme ' &
+    query = 'Trying to create a music video challenge for the month (' // trim(monthYear) &
+        // ') and would like you to return a theme ' &
         // 'for each day in the form of \"a video featuring\" followed by the theme for the day. ' &
         // 'Two days should be free choice days with no theme, and should not be on consecutive days. ' &
         // 'Themes should not exactly repeat, and should not be direct types of music. ' &
@@ -30,4 +35,41 @@ program Song
 
     print *,"Content:"
     print *,message
+
+    contains
+
+    !Get month and year (this month <= 15, next month otherwise)
+    subroutine GetMonthYear(month_and_year)
+        implicit none
+        character(len=8) :: date
+        character(len=10) :: time
+        character(len=5) :: zone
+        integer :: values(8)
+        character(len=9), dimension(12) :: monthNames
+        integer :: adjustedMonth, adjustedYear
+        character(len=9) :: adjustedMonthName
+        character(len=14),intent(out) :: month_and_year
+
+        !Array holding names of the months
+        monthNames = [ 'January  ', 'February ', 'March    ', 'April    ', 'May      ', &
+                   'June     ', 'July     ', 'August   ', 'September', 'October  ', &
+                   'November ', 'December ']
+
+        !Get current date and time
+        call date_and_time(date,time,zone,values)
+
+        !Check day to decide month to use
+        if (values(3) <= 15) then
+            adjustedMonth = values(2)
+            adjustedYear = values(1)
+        else
+            adjustedMonth = mod(values(2), 12) + 1
+            adjustedYear = values(1) + (values(2) / 12)
+        end if
+
+        ! Get the name of the adjusted month
+        adjustedMonthName = monthNames(adjustedMonth)
+
+        write(month_and_year,'(A, I0)') trim(adjustedMonthName),adjustedYear
+    end subroutine GetMonthYear
 end program Song
